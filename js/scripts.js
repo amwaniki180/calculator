d4dadcf7dc9ec83a96d9e7711a38fd3dd92d7573
Antony <amwaniki180@gmail.com>
2019-04-25 12:17:36 +0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+var add= function(number1, number2){
+return number1 + number2;
+};
+alert(add(5,5));
+
+var subtract= function(number1, number2){
+return number1 - number2;
+
+};
+alert(subtract(55,31));
+
+var add= funcion(number1, number2){
+return number1 + number2;
+}
+var number1 = prompt("enter a number:");
+alerrt(number1);
+
+var add = function(number1, number2){
+return number1 + number2;
+};
+var number1 =parseint(prompt("enter a number:"));
+var number2 =parseint(prompt("enter another number:"));
+alert(add(number1, number2));
