ddc0111576e448eb6587a1394efe847a554a6b28
Antony <amwaniki180@gmail.com>
2019-04-25 13:06:00 +0300

    created javascript file

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var add= function(number1, number2){
+var add = function(number1, number2){
 return number1 + number2;
 };
 alert(add(5,5));
@@ -9,15 +9,6 @@ return number1 - number2;
 };
 alert(subtract(55,31));
 
-var add= funcion(number1, number2){
-return number1 + number2;
-}
-var number1 = prompt("enter a number:");
-alerrt(number1);
-
-var add = function(number1, number2){
-return number1 + number2;
-};
-var number1 =parseint(prompt("enter a number:"));
-var number2 =parseint(prompt("enter another number:"));
+var number1 =parseInt(prompt("enter a number:"));
+var number2 =parseInt(prompt("enter another number:"));
 alert(add(number1, number2));
