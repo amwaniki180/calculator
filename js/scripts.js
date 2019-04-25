90111beb8b99d7a125b671c674291b1e6079f24b
Antony <amwaniki180@gmail.com>
2019-04-25 15:00:57 +0300

    multplication and division

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,16 @@ return number1 - number2;
 
 };
 alert(subtract(55,31));
+var multiply=function(number1, number2){
+return number1 * number2;
+};
+alert(multiply(6,8))
+
+var division=function(number1, number2){
+return number1 / number2;
+};
+alert(division(100,8))
+
 
 var number1 =parseInt(prompt("enter a number:"));
 var number2 =parseInt(prompt("enter another number:"));
